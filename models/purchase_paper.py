54865cb0ec2ed75543834d9415b713f34da76dcb
unknown <nguyenquocminh206@gmail.com>
2025-04-29 10:59:15 +0700

    Update4

--- models/purchase_paper.py
+++ models/purchase_paper.py
@@ -5,6 +5,10 @@ class PurchasePaper(models.Model):
 
     name = fields.Char(string='Tên')
     date = fields.Date(string='Ngày tạo', default=lambda self: fields.Date.context_today(self),readonly=True)
+    paper_ids =fields.One2many(comodel_name='purchase.order', inverse_name='purchase_id', string='Purchase',domain=[('state', 'in', ('sent', 'Chờ duyệt'))])
+    paper_count = fields.Integer(string='Số lượng phiếu', compute='_compute_paper_count',readonly=True)
+    @api.depends('paper_ids')
+    def _compute_paper_count(self):
+        for record in self:
+            record.paper_count = len(record.paper_ids)
 
-
-    paper_ids =fields.One2many(comodel_name='purchase.order', inverse_name='purchase_id', string='Purchase',domain=[('state', 'in', ('purchase', 'Chờ duyệt'))])
